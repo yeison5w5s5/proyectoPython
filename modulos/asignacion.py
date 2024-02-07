62b55dcf8a4be5bef690f63a0a0d5cbe73ff176e
yeison5w5s5 <104599687+yeison5w5s5@users.noreply.github.com>
2024-02-06 21:44:10 -0500

    feat: 🚧 Creacion de asignacin
    tengo problemas con el listado de campers y trainers

--- modulos/asignacion.py
+++ modulos/asignacion.py
@@ -11,14 +11,33 @@ def asignar():
     ruta.tbRutas()
     codRuta=str.upper(input("Escriba el codigo de la ruta: "))
     if codRuta in datos["rutas"]:
-        for i in datos["camper"]:
-            if datos["camper"][i]["Estado"]=="inscrito":
-                lista["camper"][i]=datos["camper"][i]
-        listar("{:<15}",lista["camper"])
-        cCamper=input("dijita la identificacion del camper: ")
-        if cCamper in datos["camper"]:
+        horario=input("".join([(f'{i}- {datos["horarios"][i]} \n') for i in datos["horarios"]])+"Escriba el codigo del usuario: ")
+        if horario in datos["horarios"]:
+            for i in datos["camper"]:
+                if datos["camper"][i]["Estado"]=="preinscrito":
+                    for i1 in datos["asig"]:
+                        if datos["camper"][i] not in datos["asig"][i1]["campers"]:
+                            lista["camper"][i]=datos["camper"][i]
+            listar("{:<15}",lista["camper"])
+            listacamper=[]
+            while True:
+                cCamper=input("dijita la identificacion del camper: ")
+                if cCamper in lista["camper"]:
+                    listacamper.append(cCamper)
+                else:
+                    print("cc no identificado o no disponible")
+                if continuar()==False:
+                    break
+            for b in datos["trainer"]:
+                if datos["trainer"][b]["cod_horaio"][int(horario)-1]=="1" and datos["trainer"][b]["Estado"]=="contratado":
+                    for b1 in datos["asig"]:
+                        if datos["trainer"][b] not in datos["asig"][b1]["cc_trainer"] and horario != datos["asig"][b1]["cod_horaio"]:
+                            lista["trainer"][b]=datos["trainer"][b]
+            listar("{:<15}",lista["trainer"])
             cTrainer=input("dijita la identificacion del Trainer: ")
             if cTrainer in datos["trainer"]:
+                pass
+                    
                 
 
     info={
